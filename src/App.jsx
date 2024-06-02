44db02c7dff30d7a40460178c457685916888cd3
Ebadur Rahman <ebadurshohag@gmail.com>
2024-06-02 22:47:38 +0600

    complete shorten section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
+import Shorten from "./components/Shorten";
 function App() {
   return (
     <>
       <Navbar />
       <Hero />
+      <Shorten />
     </>
   );
 }
